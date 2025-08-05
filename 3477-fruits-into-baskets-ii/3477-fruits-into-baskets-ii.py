c139670d7a66b024854c0f61a1fee0947669959e
Jhon Christian Rozano <jnrozano@up.edu.ph>
2025-08-05 19:26:57 +0800

    3477-fruits-into-baskets-ii, Easy, Time: 15 ms (84.87%), Space: 17.7 MB (96.47%)

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.py
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.py
@@ -1,24 +1,13 @@
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
-        
-        total = len(fruits)
-        basket_idx = []
-        for i in range(len(baskets)):
-            basket_idx.append(i)
-
+        count = 0
         for fruit in fruits:
-            idx = 0
-            print('fruit', fruit)
-            print('total', total)
-            print('basket', baskets[basket_idx[idx]])
-            print('basket_idx', basket_idx)
-            while basket_idx:
-                if (idx <= len(basket_idx)-1) and (baskets[basket_idx[idx]] >= fruit):
-                    basket_idx.pop(idx)
-                    total -= 1
+            unset = 1
+            for idx in range(len(baskets)):
+                if baskets[idx] >= fruit:
+                    unset = 0
+                    baskets[idx] = -1
                     break
-                elif idx > len(basket_idx)-1:
-                    break
-                idx += 1
+            count += unset
         
-        return total
+        return count
