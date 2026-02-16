bf0aff2bd4a923ba61cb0036fb66917697f42e6a
Jhon Christian Rozano <jnrozano@up.edu.ph>
2026-02-16 21:41:21 +0800

    0799-champagne-tower, Medium, Time: 153 ms (5.78%), Space: 19.9 MB (6.28%)

---
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -0,0 +1,17 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        total_rows = query_row + 2
+        dp_array = [[0.0] * total_rows for i in range(total_rows)]
+        print(dp_array)
+        dp_array[0][0] = poured
+
+        for row in range(query_row):
+            for col in range(row+1):
+                amount_to_pour = dp_array[row][col]
+                keep = min(1, amount_to_pour)
+                overflow = max(0, amount_to_pour - 1)
+                dp_array[row+1][col] += overflow / 2
+                dp_array[row+1][col+1] += overflow / 2 
+        return min(1, dp_array[query_row][query_glass])
+
+        
