a4050dae22fe21a0bb6435d7dba877895a26af0d
Jhon Christian Rozano <jhonchristianrozano@gmail.com>
2026-08-23 14:36:29 +0800

    1927-sum-game, Medium, Time: 68 ms (27.06%), Space: 19.8 MB (65.29%)

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        
+        count_of_q = 0
+        for char in num:
+            if char == '?':
+                count_of_q += 1
+        
+        print(count_of_q)
+        first_half = 0
+        second_half = 0
+        first_half_countq = 0
+        second_half_countq = 0
+
+        for char in num[:len(num) // 2]:
+            if char != '?':
+                first_half += int(char)
+            else:
+                first_half_countq += 1
+        for char in num[len(num) // 2:]:
+            if char != '?':
+                second_half += int(char)
+            else:
+                second_half_countq += 1
+            
+        if count_of_q % 2 == 1:
+            return True
+        else:
+            if first_half - second_half == 9*(second_half_countq-first_half_countq) // 2:
+                return False
+            else:
+                return True
