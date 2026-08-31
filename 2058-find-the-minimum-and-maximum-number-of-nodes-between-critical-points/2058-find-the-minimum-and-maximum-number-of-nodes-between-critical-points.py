8e43ec13770e89de3022016ebb97dbb32dfd1dc0
Jhon Christian Rozano <jhonchristianrozano@gmail.com>
2026-08-31 13:49:41 +0800

    2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points, Medium, Time: 107 ms (16.62%), Space: 63.1 MB (40.05%)

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        asn = []
+        current = head
+        prev_val = current.val
+
+        counter = 1
+        distances = []
+        while current is not None:
+            if (current.next is not None) and (current.val < prev_val and current.val < current.next.val):
+                distances.append(counter)
+            
+            if (current.next is not None) and (current.val > prev_val and current.val > current.next.val):
+                distances.append(counter)
+
+            prev_val = current.val
+            current = current.next
+            counter += 1
+
+        print(distances)
+
+        if not distances or len(distances) < 2:
+            return [-1, -1]
+
+        if len(distances) == 2:
+            return [abs(distances[1]-distances[0])] * 2
+
+        sorted_list = sorted(distances)
+        min_distances = float('inf')
+        for idx in range(1, len(distances)):
+            current = distances[idx] - distances[idx-1]
+            min_distances = min(min_distances, current)
+        return [min_distances, max(sorted_list)-min(sorted_list)]
